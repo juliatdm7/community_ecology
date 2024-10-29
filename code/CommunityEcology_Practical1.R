b097da7e0a19edc7b6243200e4f5626077764031
juliatdm7 <juliatdm7@gmail.com>
2024-10-29 18:54:02 +0000

    First commit. Preparing packages that will be used in the practical.

---
+++ code/CommunityEcology_Practical1.R
@@ -0,0 +1,11 @@
+# Comunity Ecology - Practical 1 - Biodiversity Under Pressure        29/10/2024
+
+
+install.packages("tidyverse")
+install.packages("vegan")
+install.packages("betapart")
+
+library(tidyverse)
+library(vegan)
+library(betapart)
+
